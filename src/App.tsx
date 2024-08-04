a807aa4e61cd3e3b43d07e6520579bacda7b5eaa
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2024-08-04 20:57:37 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GeneratePost from './components/GeneratePost'
+
+function App() {
+
+  return (
+    <>
+      <GeneratePost />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/GeneratePost.tsx
@@ -0,0 +1,12 @@
+
+export default function GeneratePost() {
+    return (
+        <div className="bg-[#202020] h-screen text-white">
+            <div className="max-w-[1280px] m-auto p-4">
+                <div className="border-2">
+                    Generate posts
+                </div>
+            </div>
+        </div>
+    )
+}
